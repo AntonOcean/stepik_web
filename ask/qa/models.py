9729de3d2c1bed04e7b7a310f896a9b1d202735f
AntonOcean <tmath90@yandex.ru>
2018-02-23 17:04:38 +0300

    update database

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -9,17 +9,17 @@ class QuestionManager(models.Manager):
 		return self.order_by('-rating')
 
 class Question(models.Model):
-	title = models.CharField(max_length=50)
-	text = models.TextField()
-	added_at = models.DateField()
-	rating = models.IntegerField()
-	author = models.CharField(max_length=50)
-	likes = models.ForeignKey(User)
+	title = models.CharField(default="", max_length=50)
+	text = models.TextField(default="")
+	added_at = models.DateField(null=True)
+	rating = models.IntegerField(default=0)
+	author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+	likes = models.ManyToManyField(User, related_name="q_to_likes")
 	objects = QuestionManager()
 
 class Answer(models.Model):
-	text = models.TextField()
-	added_at = models.DateField()
-	question = models.ForeignKey(Question)
-	author = models.CharField(max_length=50)
+	text = models.TextField(default="")
+	added_at = models.DateField(null=True)
+	question = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL)
+	author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 # Create your models here.
