9da7650ed55606d1998ee022f25d43ed9c463656
AntonOcean <tmath90@yandex.ru>
2018-02-23 16:31:20 +0300

    create MySql

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,5 +1,25 @@
 from __future__ import unicode_literals
-
+from django.contrib.auth.models import User
 from django.db import models
 
+class QuestionManager(models.Manager):
+	def new(self):
+		return self.order_by('-added_at')
+	def popular(self):
+		return self.order_by('-rating')
+
+class Question(models.Model):
+	title = models.CharField(max_length=50)
+	text = models.TextField()
+	added_at = models.DateField()
+	rating = models.IntegerField()
+	author = models.CharField(max_length=50)
+	likes = models.ForeignKey(User)
+	objects = QuestionManager()
+
+class Answer(models.Model):
+	text = models.TextField()
+	added_at = models.DateField()
+	question = models.ForeignKey(Question)
+	author = models.CharField(max_length=50)
 # Create your models here.
--- init.sh
+++ init.sh
@@ -3,3 +3,4 @@ sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/gunicorn.conf
 sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/gunicorn-django.conf
 sudo /etc/init.d/gunicorn restart
+sudo /etc/init.d/mysql start
