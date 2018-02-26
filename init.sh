fc10545a7ffe73fe195c95f0a0b57f9a04c2a340
AntonOcean <warres@yandex.ru>
2018-02-26 11:19:23 +0300

    One moment

--- init.sh
+++ init.sh
@@ -1,6 +1,9 @@
+sudo /etc/init.d/mysql start
+mysql -uroot -e "create database ask_db"
+python ask/manage.py makemigrations
+python ask/manage.py migrate
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/gunicorn.conf
 sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/gunicorn-django.conf
 sudo /etc/init.d/gunicorn restart
-sudo /etc/init.d/mysql start
